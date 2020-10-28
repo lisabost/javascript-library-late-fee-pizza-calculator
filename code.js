945065ab36a73aa9f3175b37820a1ec2e739f393
lisabost <lbost@my.wctc.edu>
2020-10-28 15:47:25 -0500

    updated code to use jQuery - no more alerts and prompts hooray!

--- code.js
+++ code.js
@@ -1,46 +1,86 @@
+$(document).ready(function() {
 
-function calculateLateFee() {
-    var bookLateFee = 0.25;
-    var DVDLateFee = 0.50;
-    // get number of books that were late
-    var lateBooks = parseInt(prompt("How many books are late?"));
-    // get number of DVDs that were late
-    var lateDVD = parseInt(prompt("How many DVDs are late?"));
-    // get number of days late
-    var daysLate = parseInt(prompt("How many days are the items late?"));
-    // do math for books - number of days books were late * book late fee * number of books
-    var booksLateFee = daysLate * bookLateFee * lateBooks;
-    //make formatted display version of number
-    var prettyBLateFee = booksLateFee.toFixed(2);
-    // do math for DVDs - number of days books were late * DVD late fee * number of DVDs
-    var DVDsLateFee = daysLate * DVDLateFee * lateDVD;
-    //make formatted display version of number
-    var prettyDLateFee = DVDsLateFee.toFixed(2);
-    // add individual fees together
-    var totalLateFee = (booksLateFee + DVDsLateFee).toFixed(2);
-    // Display book late fee
-    alert(`The late fee for your books is $${prettyBLateFee}`);
-    // Display DVD late fee
-    alert(`The late fee for your DVDs is $${prettyDLateFee}`);
-    // Display total late fee
-    alert(`Your total late fee is $${totalLateFee}`);
-}
+    $("#fineButton").click(calculateLateFee);
+    $("#pizzaButton").click(calculatePizzaPrice);
 
-function calculatePizzaPrice() {
-    var basePriceOfPizza = 15.00;
-    var toppingPrice = 1.25;
-    //get number of toppings added to pizza
-    var numToppings = parseInt(prompt("How many toppings are you getting?"));
-    // get number of coworkers eating pizza
-    var numCoworkers = parseInt(prompt("How many people are having the pizza?"))
-    // do math for total pizza - 15 + 1.25 * number of toppings
-    var totalPizzaPrice = basePriceOfPizza + (numToppings * toppingPrice);
-    // do math for pizza per person- total pizza price / number of coworkers
-    var pricePerPerson = totalPizzaPrice / numCoworkers;
-    //Display total cost of pizza
-    var displayTotalPizzaPrice = totalPizzaPrice.toFixed(2);
-    alert(`The total cost of the pizza is $${displayTotalPizzaPrice}`);
-    //Display price per person
-    var displayPricePerPerson = pricePerPerson.toFixed(2);
-    alert(`The price per person for the pizza is $${displayPricePerPerson}`);
-}
+    function calculateLateFee() {
+        var bookLateFee = 0.25;
+        var DVDLateFee = 0.50;
+
+        /*
+        Gather input from the webpage and parse to number data type
+         */
+        //get value from box with id lateBooks
+        var lateBooks = parseInt($("#lateBooks").val());
+
+        // get number of DVDs that were late
+        var lateDVD = parseInt($("#lateDVD").val());
+
+        // get number of days late
+        var daysLate = parseInt($("#numDaysLate").val());
+
+        /*
+        Do math and display output
+         */
+
+        //calculate book late fees
+        var booksLateFee = daysLate * bookLateFee * lateBooks;
+        //make formatted display version of number
+        var bookFeeDisplay = booksLateFee.toFixed(2);
+
+        // calculate late fee for DVDs
+        var DVDsLateFee = daysLate * DVDLateFee * lateDVD;
+        //make formatted display version of number
+        var DvdFeeDisplay = DVDsLateFee.toFixed(2);
+
+        // add individual fees together
+        var totalLateFee = (booksLateFee + DVDsLateFee).toFixed(2);
+
+        // Update text in span with id=bookLateFee
+        $("#bookLateFee").text(bookFeeDisplay);
+        // Update text in span with id=DvdLateFee
+        $("#DvdLateFee").text(DvdFeeDisplay);
+        //Update text in span with id=totalLateFee
+        $("#totalLateFee").text(totalLateFee);
+        //Un-hide the output div
+        $(".output").show();
+
+    }
+
+    function calculatePizzaPrice() {
+        var basePriceOfPizza = 15.00;
+        var toppingPrice = 1.25;
+
+        /*
+        Gather information from webpage and parse to number data type
+         */
+
+        //get number of toppings added to pizza
+        var numToppings = parseInt($("#toppings").val());
+
+        // get number of coworkers eating pizza
+        var numCoworkers = parseInt($("#peopleToShare").val());
+
+        /*
+        Do the math and make display versions with 2 decimal places
+         */
+        // do math for total pizza
+        var totalPizzaPrice = basePriceOfPizza + (numToppings * toppingPrice);
+        var displayTotalPizzaPrice = totalPizzaPrice.toFixed(2);
+
+        // do math for pizza per person
+        var pricePerPerson = totalPizzaPrice / numCoworkers;
+        var displayPricePerPerson = pricePerPerson.toFixed(2);
+
+        /*
+        Output display
+         */
+        //Update text in span with id=totalPizzaPrice
+        $("#totalPizzaPrice").text(displayTotalPizzaPrice);
+        //Update text in span with id=pricePerPerson
+        $("#pricePerPerson").text(displayPricePerPerson);
+        //un-hide output display for pizza calculator
+        $(".pizzaOutput").show();
+    }
+
+});
